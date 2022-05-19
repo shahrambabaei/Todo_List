e16124028f82e92d9615a4f7e9f90696fec80330
shahram <shahram.b6996@gmail.com>
2022-05-19 15:56:41 +0430

    Add Ability SearchTask in Projrct

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -19,64 +19,128 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final box = Hive.box<Task>(taskBoxName);
   late ThemeData themeData;
+  late TextEditingController _controller;
+  late ValueNotifier<String> searchValueNotifier;
   @override
   void didChangeDependencies() {
     themeData = Theme.of(context);
+    _controller = TextEditingController();
+    searchValueNotifier = ValueNotifier('');
     super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar:
-            HomeAppBar(width: MediaQuery.of(context).size.width, height: 110),
-        body: ValueListenableBuilder<Box<Task>>(
-            valueListenable: box.listenable(),
-            builder: (context, box, widget) {
-              if (box.isNotEmpty) {
-                return ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  padding: const EdgeInsets.fromLTRB(16, 10, 16, 80),
-                  itemCount: box.values.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return const HeaderListView();
-                    } else {
-                      final Task task = box.values.toList()[index - 1];
-                      return BodyListView(
-                        task: task,
-                      );
-                      // Text(task.text);
-                    }
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            height: 102,
+            padding: const EdgeInsets.fromLTRB(16, 10, 16, 5),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              themeData.colorScheme.primary,
+              themeData.colorScheme.primaryContainer
+            ])),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('To Do List',
+                      style: themeData.textTheme.headline6!
+                          .apply(color: themeData.colorScheme.onPrimary)),
+                  Icon(
+                    Icons.menu,
+                    color: themeData.colorScheme.onPrimary,
+                  )
+                ],
+              ),
+              Container(
+                height: 40,
+                margin: const EdgeInsets.only(top: 15),
+                child: TextField(
+                  controller: _controller,
+                  onChanged: (value) {
+                    searchValueNotifier.value = _controller.text;
                   },
-                );
-              } else {
-                return emptyState();
-              }
-            }),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EditTaskScreen(task: Task()),
-                  ));
-            },
-            label: Row(
-              children: const [
-                Text('Add New tasks'),
-                SizedBox(
-                  width: 8,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(0),
+                      prefixIcon:
+                          const Icon(Icons.search, color: secondaryTextColor),
+                      hintText: 'search tasks...',
+                      hintStyle: const TextStyle(color: secondaryTextColor),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                          borderSide: BorderSide.none),
+                      filled: true,
+                      fillColor: Colors.white),
                 ),
-                FloatingActionButtonWidgets(icon: Icons.add)
-              ],
-            )),
-      ),
+              )
+            ]),
+          ),
+          Expanded(
+              child: ValueListenableBuilder<String>(
+                  valueListenable: searchValueNotifier,
+                  builder: (context, value, widget) {
+                    return ValueListenableBuilder<Box<Task>>(
+                        valueListenable: box.listenable(),
+                        builder: (context, box, widget) {
+                          final List<Task> items;
+                          if(_controller.text.isEmpty){
+                            items=box.values.toList();
+
+                          }else{
+                            items=box.values.where((element) => element.text.contains(_controller.text)).toList();
+                          }
+                          if (items.isNotEmpty) {
+                            return ListView.builder(
+                              physics: const BouncingScrollPhysics(),
+                              padding:
+                                  const EdgeInsets.fromLTRB(16, 10, 16, 80),
+                              itemCount:items.length + 1,
+                              itemBuilder: (context, index) {
+                                if (index == 0) {
+                                  return const HeaderListView();
+                                } else {
+                                  final Task task =
+                                      items[index - 1];
+                                  return BodyListView(
+                                    task: task,
+                                  );
+                                  // Text(task.text);
+                                }
+                              },
+                            );
+                          } else {
+                            return emptyState();
+                          }
+                        });
+                  }))
+        ],
+      )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditTaskScreen(task: Task()),
+                ));
+          },
+          label: Row(
+            children: const [
+              Text('Add New tasks'),
+              SizedBox(
+                width: 8,
+              ),
+              FloatingActionButtonWidgets(icon: Icons.add)
+            ],
+          )),
     );
   }
 
+//
   Widget emptyState() {
     return Center(
       child: Column(
@@ -87,10 +151,10 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 120,
             width: 120,
           ),
-          SizedBox(
+          const SizedBox(
             height: 10,
           ),
-          Text('Your task List is empty')
+          const Text('Your task List is empty')
         ],
       ),
     );
--- lib/widgets/homeappbar.dart
+++ lib/widgets/homeappbar.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/main.dart';
 
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final double width, height;
-  const HomeAppBar({Key? key, required this.width, required this.height})
-      : super(key: key);
+class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const HomeAppBar({Key? key}) : super(key: key);
+
+  @override
+  State<HomeAppBar> createState() => _HomeAppBarState();
+
+  @override
+  Size get preferredSize => const Size(double.infinity, 100);
+}
+
+class _HomeAppBarState extends State<HomeAppBar> {
+  late ThemeData themeData;
+  late TextEditingController _controller;
+  static final ValueNotifier<String> searchValueNotifier = ValueNotifier('');
+  @override
+  void didChangeDependencies() {
+    themeData = Theme.of(context);
+    _controller = TextEditingController();
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
     return Container(
       height: 102,
       padding: const EdgeInsets.fromLTRB(16, 10, 16, 5),
@@ -34,6 +49,10 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
           height: 40,
           margin: const EdgeInsets.only(top: 15),
           child: TextField(
+            controller: _controller,
+            onChanged: (value) {
+              searchValueNotifier.value = _controller.text;
+            },
             decoration: InputDecoration(
                 contentPadding: const EdgeInsets.all(0),
                 prefixIcon: const Icon(Icons.search, color: secondaryTextColor),
@@ -49,7 +68,4 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
       ]),
     );
   }
-
-  @override
-  Size get preferredSize => Size(width, height);
 }
